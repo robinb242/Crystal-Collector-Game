cdb33740cd06973441f9723a51467550d8686766
Robin Bertuccelli <robin.bertuccelli@gmail.com>
2017-12-12 12:00:14 -0600

    My started code

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+
+
+
+
+$(document).ready(function() {
+
+
+//GLOBAL VARIABLES
+//********************************************************************
+
+var randomTargetNumber = "[]";
+var crystalValues =[
+	"crystal1",
+	"crystal2",
+	"crystal3",
+	"crystal4"
+]
+
+var selectedCrystal=0;
+var guesssTotal = 0:
+var winsCount =0;
+var lossesCount= 0;
+
+
+
+//FUNCTIONS (reusable blocks of code to be recalled)
+//*********************************************************************
+//Start the Game
+function startGame (){
+
+ 		 randomTargetNumber = Math.floor(Math.random() * (120-19+1)
+ 
+ 	//TEST
+ 	console.log(randomTargetNumber);
+
+}
+//need four images with their own IDs to act as containers to hold 
+//the hidden numbers.
+
+
+
+//random number generator for randomTargetNumber, and each randomHiddenNumber
+
+//Onclick event to add  userGuess to guessesTotal
+
+//Add the selected crystal value to the total.
+
+//Win/Loose for loop
+
+//win event, what happens when you hit the target number.
+
+//Loose event, what happens when you go evr the total
+
+
+
+//MAIN PROCESSES (call upon the functions to make something happen)
+//*************************************************************************
+
+//Ends document reeady function
+});
